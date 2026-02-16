a218ea4f11f0e2062560e7ce6cd1d380312f7612
Pablo Wolter <pwolter@gmail.com>
2026-02-15 17:27:22 -0800

    Update the config files

---
+++ nvim/lua/plugins/telescope-nvim.lua
@@ -0,0 +1,40 @@
+-- Fuzzy finder
+return {
+  -- https://github.com/nvim-telescope/telescope.nvim
+  'nvim-telescope/telescope.nvim',
+  lazy = true,
+  dependencies = {
+    -- https://github.com/nvim-lua/plenary.nvim
+    { 'nvim-lua/plenary.nvim' },
+    {
+      -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
+    },
+    { 'nvim-tree/nvim-web-devicons' },
+    { 'falke/todo-comments.nvim' },
+  },
+  opts = {
+    defaults = {
+      layout_config = {
+        vertical = {
+          width = 0.75
+        }
+      },
+      path_display = {
+        filename_first = {
+          reverse_directories = true
+        }
+      },
+      mappings = {
+        n = {
+          ["d"] = require("telescope.actions").delete_buffer,
+          ["q"] = require("telescope.actions").close,
+        },
+      },
+    }
+  }
+}
